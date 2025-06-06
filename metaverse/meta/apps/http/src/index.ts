c3c8a1fa09ff78939a734fe1e7acbacb90f9a0a4
sunny <rajputsny2@gmail.com>
2025-06-07 01:32:09 +0530

    Initial commit of existing project

---
+++ metaverse/meta/apps/http/src/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { router } from "./routes/v1";
+import client from "@repo/db/client";
+const app = express();
+app.use(express.json());//to parse body
+app.use("/api/v1",router);
+
+app.listen(process.env.PORT || 3000) 
