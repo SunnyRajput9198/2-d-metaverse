e94c13fe6a61a7a2db79a8ee993802ced12a0bb2
sunny <rajputsny2@gmail.com>
2025-10-03 23:23:30 +0530

    added process.env.NEXT_PUBLIC_API_URL

--- metaverse/meta/apps/frontend/src/config.ts
+++ metaverse/meta/apps/frontend/src/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL ||"http://localhost:3000";
 export const WS_URL = "ws://localhost:3001";
